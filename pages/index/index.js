0fc90caa59fb0895b60cca49a7b57867d111a433
MrSeaWave <MrDaemon@outlook.com>
2019-07-24 22:12:11 +0800

    feat : copy 1024

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,254 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+var util = require("../../utils/util.js");
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    chessboardDatas: [[2, 0, 2, 0], [0, 2, 0, 0], [2, 4, 2, 0], [0, 0, 2, 4]],
+    current_score: 0,
+    best_score: 0,
+    toast2Hidden: true
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  getCurrentBestScore: function() {
+    let best_score = this.data.best_score;
+    const current_score = this.data.current_score;
+
+    best_score = current_score > best_score ? current_score : best_score;
+    return best_score;
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+  judgeSuccess: function() {
+    const { chessboardDatas } = this.data;
+
+    for (let i = 0; i < chessboardDatas.length; i++) {
+      for (let j = 0; j < chessboardDatas[i].length; j++) {
+        if (chessboardDatas[i][j] >= 2048) {
+          this.recordState();
           this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+            toast2Hidden: false
+          });
+          break;
         }
-      })
+      }
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  recordState: function() {
+    const best_score = this.getCurrentBestScore();
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      best_score: best_score
+    });
+    wx.setStorageSync("uncompleteState", this.data);
+  },
+  reset: function() {
+    const chessDefaultDatas = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    let maxInitNum = util.getRandomNum(1, 8);
+    while (maxInitNum > 0) {
+      const num = util.getRandomNum(0, 15);
+      this.setChessboardCellNum(chessDefaultDatas, num, 2);
+      maxInitNum--;
+    }
+
+    this.setData({
+      chessboardDatas: chessDefaultDatas,
+      current_score: 0,
+      toast2Hidden: true
+    });
+  },
+  getChessboardCellNum: function(array, index) {
+    let loopCount = 0;
+    let cell;
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+        if (index === loopCount++) {
+          cell = array[i][j];
+        }
+      }
+    }
+    return cell;
+  },
+  setChessboardCellNum: function(array, index, num) {
+    let loopCount = 0;
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+        if (index === loopCount++) {
+          array[i][j] = num;
+        }
+      }
+    }
+  },
+  generateNewCellNum: function(chessboardDatas) {
+    // 1.求出所有剩余空元素
+    let remainNullCellNum = 0;
+    for (let i = 0; i < chessboardDatas.length; i++) {
+      for (let j = 0; j < chessboardDatas[i].length; j++) {
+        if (chessboardDatas[i][j] === 0) {
+          remainNullCellNum++;
+        }
+      }
+    }
+    // 2.随机位置产生一个数
+    let newCellNumIndex = util.getRandomNum(0, remainNullCellNum);
+    let count = 0;
+    for (let i = chessboardDatas.length - 1; i >= 0; i--) {
+      for (let j = chessboardDatas[i].length - 1; j >= 0; j--) {
+        if (chessboardDatas[i][j] !== 0) {
+          continue;
+        }
+        if (newCellNumIndex === count) {
+          chessboardDatas[i][j] = 2;
+        }
+        count++;
+      }
+    }
+  },
+  onLoad: function(options) {
+    let data = wx.getStorageSync("uncompleteState");
+    if (!!data) {
+      this.setData(data);
+    }
+  },
+  onUnload: function() {
+    this.recordState();
+  },
+  onHide: function() {
+    this.recordState();
+  },
+  turnEnd: function(chessboardDatas, addScore) {
+    this.generateNewCellNum(chessboardDatas);
+
+    let best_score = this.getCurrentBestScore();
+    this.setData({
+      chessboardDatas: chessboardDatas,
+      current_score: this.data.current_score + addScore,
+      best_score: best_score
+    });
+
+    this.judgeSuccess();
+
+    util.scanArray(this.data.chessboardDatas);
+  },
+  turnUp: function() {
+    let chessboardDatas = this.data.chessboardDatas;
+    let addScore = 0;
+    this.reorder_up(chessboardDatas);
+    for (let i = 0; i < chessboardDatas.length - 1; i++) {
+      for (let j = 0; j < chessboardDatas[i].length; j++) {
+        if (chessboardDatas[i][j] === chessboardDatas[i + 1][j]) {
+          addScore += chessboardDatas[i][j];
+          chessboardDatas[i][j] =
+            chessboardDatas[i][j] + chessboardDatas[i + 1][j];
+          chessboardDatas[i + 1][j] = 0;
+        }
+      }
+    }
+    this.reorder_up(chessboardDatas);
+    this.turnEnd(chessboardDatas, addScore);
+  },
+  turnDown: function() {
+    let chessboardDatas = this.data.chessboardDatas;
+    let addScore = 0;
+    this.reorder_down(chessboardDatas);
+    for (let i = chessboardDatas.length - 1; i >= 1; i--) {
+      for (let j = chessboardDatas[i].length - 1; j >= 0; j--) {
+        if (chessboardDatas[i][j] === chessboardDatas[i - 1][j]) {
+          addScore += chessboardDatas[i][j];
+          chessboardDatas[i][j] =
+            chessboardDatas[i][j] + chessboardDatas[i - 1][j];
+          chessboardDatas[i - 1][j] = 0;
+        }
+      }
+    }
+    this.reorder_down(chessboardDatas);
+    this.turnEnd(chessboardDatas, addScore);
+  },
+  turnLeft: function() {
+    let chessboardDatas = this.data.chessboardDatas;
+    let addScore = 0;
+    this.reorder_left(chessboardDatas);
+    for (let j = 0; j < chessboardDatas.length - 1; j++) {
+      for (let i = 0; i < chessboardDatas.length; i++) {
+        if (chessboardDatas[i][j] === chessboardDatas[i][j + 1]) {
+          addScore += chessboardDatas[i][j];
+          chessboardDatas[i][j] =
+            chessboardDatas[i][j] + chessboardDatas[i][j + 1];
+          chessboardDatas[i][j + 1] = 0;
+        }
+      }
+    }
+    this.reorder_left(chessboardDatas);
+    this.turnEnd(chessboardDatas, addScore);
+  },
+  turnRight: function() {
+    let addScore = 0;
+    let chessboardDatas = this.data.chessboardDatas;
+    this.reorder_right(chessboardDatas);
+    for (let j = chessboardDatas.length - 1; j >= 0; j--) {
+      for (let i = chessboardDatas.length - 1; i >= 1; i--) {
+        if (chessboardDatas[i][j] === chessboardDatas[i][j - 1]) {
+          addScore += chessboardDatas[i][j];
+          chessboardDatas[i][j] =
+            chessboardDatas[i][j] + chessboardDatas[i][j - 1];
+          chessboardDatas[i][j - 1] = 0;
+        }
+      }
+    }
+    this.reorder_right(chessboardDatas);
+    this.turnEnd(chessboardDatas, addScore);
+  },
+  reorder_up: function(chessboardDatas) {
+    for (let i = 0; i < chessboardDatas.length; i++) {
+      for (let j = 0; j < chessboardDatas[i].length; j++) {
+        let rowIndex = i;
+        while (rowIndex - 1 >= 0 && chessboardDatas[rowIndex - 1][j] === 0) {
+          chessboardDatas[rowIndex - 1][j] = chessboardDatas[rowIndex][j];
+          chessboardDatas[rowIndex][j] = 0;
+          rowIndex--;
+        }
+      }
+    }
+  },
+  reorder_down: function(chessboardDatas) {
+    for (let i = chessboardDatas.length - 2; i >= 0; i--) {
+      for (let j = chessboardDatas[i].length - 1; j >= 0; j--) {
+        let rowIndex = i;
+        while (
+          rowIndex + 1 <= chessboardDatas.length - 1 &&
+          chessboardDatas[rowIndex + 1][j] === 0
+        ) {
+          chessboardDatas[rowIndex + 1][j] = chessboardDatas[rowIndex][j];
+          chessboardDatas[rowIndex][j] = 0;
+          rowIndex++;
+        }
+      }
+    }
+  },
+  reorder_left: function(chessboardDatas) {
+    for (let j = 0; j < chessboardDatas.length; j++) {
+      for (let i = 0; i < chessboardDatas[j].length; i++) {
+        let rowIndex = j;
+        while (rowIndex - 1 >= 0 && chessboardDatas[i][rowIndex - 1] === 0) {
+          chessboardDatas[i][rowIndex - 1] = chessboardDatas[i][rowIndex];
+          chessboardDatas[i][rowIndex] = 0;
+          rowIndex--;
+        }
+      }
+    }
+  },
+  reorder_right: function(chessboardDatas) {
+    for (let j = chessboardDatas.length - 2; j >= 0; j--) {
+      for (let i = chessboardDatas.length - 1; i >= 0; i--) {
+        let rowIndex = j;
+        while (
+          rowIndex + 1 <= chessboardDatas.length - 1 &&
+          chessboardDatas[i][rowIndex + 1] === 0
+        ) {
+          chessboardDatas[i][rowIndex + 1] = chessboardDatas[i][rowIndex];
+          chessboardDatas[i][rowIndex] = 0;
+          rowIndex++;
+        }
+      }
+    }
   }
-})
+});
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,50 @@
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
+  return (
+    [year, month, day].map(formatNumber).join("/") +
+    " " +
+    [hour, minute, second].map(formatNumber).join(":")
+  );
+};
 
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+  n = n.toString();
+  return n[1] ? n : "0" + n;
+};
+
+const getRandomNum = (Min, Max) => {
+  const Range = Max - Min;
+  const Rand = Math.random();
+  return Min + Math.round(Rand * Range);
+};
+
+const swapItems = (arr, index1, index2) => {
+  arr[index1] = arr.splice(index2, 1, arr[index1])[0];
+  return arr;
+};
+
+const scanArray = arr => {
+  let str = "";
+  for (let key in arr) {
+    if (arr[key] instanceof Array || typeof arr[key] === "object") {
+      // 递归调用
+      scanArray(arr[key]);
+    } else {
+      str = str + " " + arr[key];
+    }
+  }
+  console.log(str);
+};
 
 module.exports = {
-  formatTime: formatTime
-}
+  formatTime: formatTime,
+  getRandomNum: getRandomNum,
+  swapItems: swapItems,
+  scanArray: scanArray
+};
